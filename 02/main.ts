3d89b5756dc5633d8fd1aeb69da8e87a29c05593
hugohabicht01 <hugohabicht01@gmx.net>
2021-12-05 13:38:29 +0000

    First part day 2

---
+++ 02/main.ts
@@ -0,0 +1,73 @@
+import { example, input } from './input';
+
+enum EInstruction {
+  forward = 'forward',
+  down = 'down',
+  up = 'up'
+}
+
+interface Instruction {
+  instruction: EInstruction;
+  amount: number
+}
+
+class Instruction {
+  constructor(instruction: string) {
+    const [inst, amo] = instruction.split(' ')
+    switch (inst) {
+      case EInstruction.down:
+        this.instruction = EInstruction.down;
+        break;
+      case EInstruction.forward:
+        this.instruction = EInstruction.forward;
+        break;
+      case EInstruction.up:
+        this.instruction = EInstruction.up;
+        break;
+    }
+    this.amount = parseInt(amo);
+  }
+}
+
+interface Submarine {
+  instructions: Instruction[];
+  horizontalPos: number;
+  depth: number;
+}
+
+class Submarine {
+  constructor(instructions: string) {
+    this.instructions = instructions.split('\n').map(el => new Instruction(el))
+    this.horizontalPos = 0
+    this.depth = 0;
+  }
+
+  runAll() {
+    for (const instruction of this.instructions) {
+      this.runInstruction(instruction);
+    }
+  }
+
+  runInstruction(instruction: Instruction) {
+    switch (instruction.instruction) {
+      case EInstruction.down:
+        this.depth += instruction.amount;
+        break;
+      case EInstruction.up:
+        this.depth -= instruction.amount;
+        break;
+      case EInstruction.forward:
+        this.horizontalPos += instruction.amount;
+        break;
+    }
+  }
+
+  product() {
+    return this.depth * this.horizontalPos;
+  }
+}
+
+const sub = new Submarine(input);
+
+sub.runAll()
+console.log(sub.product())
