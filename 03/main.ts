512985acf9b8e21789f5da8cae211cf3ad7ef6be
hugohabicht01 <hugohabicht01@gmx.net>
2021-12-05 17:13:45 +0000

    Part 2 on 03

--- 03/main.ts
+++ 03/main.ts
@@ -5,13 +5,38 @@ let data = input.split("\n");
 let gamma = [];
 let epsilon = [];
 
-for (let i = 0;i < data[0].length;i++) {
-  const bits = data.map(line => line[i])
-  const mostSignificantBit = bits.filter(bit => bit === "1").length > bits.filter(bit => bit === "0").length ? '1' : '0';
-  const leastSignificantBit = bits.filter(bit => bit === "1").length < bits.filter(bit => bit === "0").length ? '1' : '0';
-  gamma.push(mostSignificantBit);
-  epsilon.push(leastSignificantBit);
+const findBits = (data: string[], index: number) => {
+  const bits = data.map(line => line[index]);
+  const mostSignificantBit = bits.filter(bit => bit === '1').length >= bits.filter(bit => bit === '0').length ? '1' : '0';
+  const leastSignificantBit = bits.filter(bit => bit === '0').length <= bits.filter(bit => bit === '1').length ? '0' : '1';
+  return [mostSignificantBit, leastSignificantBit];
 }
 
-const power = parseInt(gamma.join(""), 2) * parseInt(epsilon.join(""), 2);
-console.log(power);
+// Part 1
+// for (let i = 0; i < data[0].length; i++) {
+//   const bits = data.map(line => line[i])
+//   const mostSignificantBit = bits.filter(bit => bit === "1").length > bits.filter(bit => bit === "0").length ? '1' : '0';
+//   const leastSignificantBit = bits.filter(bit => bit === "1").length < bits.filter(bit => bit === "0").length ? '1' : '0';
+//   gamma.push(mostSignificantBit);
+//   epsilon.push(leastSignificantBit);
+// }
+
+// const power = parseInt(gamma.join(""), 2) * parseInt(epsilon.join(""), 2);
+// console.log(power);
+
+let oxygenData = data.slice();
+let co2Data = data.slice();
+
+for (let i = 0; i < data[0].length; i++) {
+  if (oxygenData.length > 1) {
+    const [most,] = findBits(oxygenData, i);
+    oxygenData = oxygenData.filter(line => line[i] === most);
+  }
+  if (co2Data.length > 1) {
+    const [, least] = findBits(co2Data, i);
+    co2Data = co2Data.filter(line => line[i] === least);
+  }
+}
+
+const result = parseInt(oxygenData[0], 2) * parseInt(co2Data[0], 2);
+console.log(result);
